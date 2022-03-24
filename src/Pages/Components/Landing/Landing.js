56d0528e3d4ca38b2eac58b2e0be8a9842b1c2d3
anshul12345hp <93330838+anshul12345hp@users.noreply.github.com>
2022-03-24 20:14:31 +0530

    Update Landing.js

--- src/Pages/Components/Landing/Landing.js
+++ src/Pages/Components/Landing/Landing.js
@@ -10,7 +10,7 @@ function Landing() {
         <div className="logo-murious">
           <img src={Logo}/>
           <CountdownTimer
-            countdownTimestampMs={72000000}/>
+            countdownTimestampMs={1647991800}/>
         </div>
         <img src={Globe} className="globe-1"/>
         <img src={Globe} className="globe-2"/>
