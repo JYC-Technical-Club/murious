f8a09b87f15e39515a120498cb9c0e799b399d80
dwag-cyber <tg1570@gmail.com>
2025-02-12 09:04:56 +0530

    new

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -58,7 +58,7 @@ const teamleads = [
         "id": 2,
         "name": "Aarya Gupta",
         "designation": "Mentor",
-        "imageUrl": "./team leads/aarya.webp",
+        "imageUrl": "./team leads/aaryaa.webp",
         "linkedinUrl": "https://www.linkedin.com/in/aarya--gupta/",
         "instagramUrl": "https://www.instagram.com/aaryaa_.0/"
     },
